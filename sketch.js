bbdd433a5c28e168b313f7ed87900a009688c6c3
BuildTools <unconfigured@null.spigotmc.org>
2025-03-05 17:12:53 +0100

    Lavet figuren

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+function preload() {
+	baggrund = loadImage('baggrund.png');
+  }
+
 function setup(){
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(1800, 900);
+	background(baggrund)
 }
 
 function draw(){
+strokeWeight(10)
+line(200, 700, 250, 870) //højre ben
+line(200, 700, 150, 870) //venstreben 
+line(200, 500, 200, 700) //krop
+fill(255, 191, 117)
+circle(200, 450, 150) //hoved
+
+//selve armen!!
+line(200, 550, 350, 500)
+
 
 }
